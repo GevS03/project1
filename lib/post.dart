639c4ac6bb48e7d83fa20971df45f7b7f352811d
GevS03 <gevorg03stepanyan>
2023-08-22 19:14:55 +0400

    adding body

---
+++ lib/post.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Post {
+  final String accountName;
+  final String city;
+  final String country;
+  List<Image> pictures;
+  String description;
+  String hashtags;
+  int picturesNum;
+
+  Post(
+      {required this.accountName,
+      required this.city,
+      required this.country,
+      required this.pictures,
+      required this.picturesNum,
+      required this.description,
+      required this.hashtags});
+}
